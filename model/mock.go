96095e6f8c15e5418c0096f2edb16ec4641a0d7d
Dennis Dantas <ddspog@gmail.com>
2017-11-13 13:45:58 -0200

    style(model mock): Ref unneded public names to private :art:
    I've modified some names that didn't need to be public, to use private
    scope, using interfaces for publishing some needed values.
    
    Closes #3

--- model/mock.go
+++ model/mock.go
@@ -10,8 +10,8 @@ import (
 // MockModelSetup it's a setup type for configuring functions mocking.
 type MockModelSetup struct {
 	originalNowInMilli func() int64
-	tempNowInMilli     *FakeNowInMilli
-	tempNewID          *FakeNewID
+	tempNowInMilli     *fakeNowInMilli
+	tempNewID          *fakeNewID
 }
 
 // NewMockModelSetup returns a new MockModelSetup, already configuring
@@ -23,8 +23,8 @@ func NewMockModelSetup(t *testing.T) (s *MockModelSetup, err error) {
 	} else {
 		s = &MockModelSetup{
 			originalNowInMilli: nowInMilli,
-			tempNowInMilli:     NewFakeNowInMilli(),
-			tempNewID:          NewFakeNewID(),
+			tempNowInMilli:     newFakeNowInMilli(),
+			tempNewID:          newFakeNewID(),
 		}
 		s.tempNowInMilli.mockModelSetupP = s
 		s.tempNewID.mockModelSetupP = s
@@ -38,50 +38,56 @@ func (s *MockModelSetup) Finish() {
 }
 
 // NowInMilli returns the fake NowInMilli object on this setup.
-func (s *MockModelSetup) NowInMilli() (f *FakeNowInMilli) {
+func (s *MockModelSetup) NowInMilli() (f FakeNowInMillier) {
 	f = s.tempNowInMilli
 	return
 }
 
 // NewID returns the fake NewID object on this setup.
-func (s *MockModelSetup) NewID() (f *FakeNewID) {
+func (s *MockModelSetup) NewID() (f FakeNewIDer) {
 	f = s.tempNewID
 	return
 }
 
 // updateNowInMilli the nowInMilli function with a mocked one.
 func (s *MockModelSetup) updateNowInMilli() {
-	nowInMilli = s.NowInMilli().getFunction()
+	nowInMilli = s.NowInMilli().GetFunction()
 }
 
 // updateNewID the NewID function with a mocked one.
 func (s *MockModelSetup) updateNewID() {
-	newID = s.NewID().getFunction()
+	newID = s.NewID().GetFunction()
 }
 
-// FakeNowInMilli it's a type that enable mocking of function nowInMilli.
-type FakeNowInMilli struct {
+// fakeNowInMilli it's a type that enable mocking of function nowInMilli.
+type fakeNowInMilli struct {
 	returnV         *int64
 	mockModelSetupP *MockModelSetup
 }
 
-// NewFakeNowInMilli returns a new FakeNowInMilli object.
-func NewFakeNowInMilli() (f *FakeNowInMilli) {
+// FakeNowInMillier it's a function mocking object, needed for mock purposes.
+type FakeNowInMillier interface {
+	Returns(int64)
+	GetFunction() func() int64
+}
+
+// newFakeNowInMilli returns a new FakeNowInMilli object.
+func newFakeNowInMilli() (f *fakeNowInMilli) {
 	var i int64
-	f = &FakeNowInMilli{
+	f = &fakeNowInMilli{
 		returnV: &i,
 	}
 	return
 }
 
 // Returns ensures a value to be returned on calls to nowInMilli.
-func (f *FakeNowInMilli) Returns(t int64) {
+func (f *fakeNowInMilli) Returns(t int64) {
 	*f.returnV = t
 	f.mockModelSetupP.updateNowInMilli()
 }
 
 // getFunction creates a version of nowInMilli that returns value demanded.
-func (f *FakeNowInMilli) getFunction() (fn func() int64) {
+func (f *fakeNowInMilli) GetFunction() (fn func() int64) {
 	fn = func() (t int64) {
 		t = *f.returnV
 		return
@@ -89,29 +95,35 @@ func (f *FakeNowInMilli) getFunction() (fn func() int64) {
 	return
 }
 
-// FakeNewID it's a type that enable mocking of function NewID.
-type FakeNewID struct {
+// fakeNewID it's a type that enable mocking of function NewID.
+type fakeNewID struct {
 	returnV         *bson.ObjectId
 	mockModelSetupP *MockModelSetup
 }
 
-// NewFakeNewID returns a new FakeNewID object.
-func NewFakeNewID() (f *FakeNewID) {
+// FakeNewIDer it's a function mocking object, needed for mock purposes.
+type FakeNewIDer interface {
+	Returns(bson.ObjectId)
+	GetFunction() func() bson.ObjectId
+}
+
+// newFakeNewID returns a new FakeNewID object.
+func newFakeNewID() (f *fakeNewID) {
 	var id = newID()
-	f = &FakeNewID{
+	f = &fakeNewID{
 		returnV: &id,
 	}
 	return
 }
 
 // Returns ensures a value to be returned on calls to NewID.
-func (f *FakeNewID) Returns(id bson.ObjectId) {
+func (f *fakeNewID) Returns(id bson.ObjectId) {
 	*f.returnV = id
 	f.mockModelSetupP.updateNewID()
 }
 
 // getFunction creates a version of NewID that returns value demanded.
-func (f *FakeNewID) getFunction() (fn func() bson.ObjectId) {
+func (f *fakeNewID) GetFunction() (fn func() bson.ObjectId) {
 	fn = func() (id bson.ObjectId) {
 		id = *f.returnV
 		return
