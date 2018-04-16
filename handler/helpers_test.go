163fd30580f2f04450f9751e67c473562e8c2304
Dênnis Dantas <ddspog@gmail.com>
2018-04-16 04:15:03 -0300

    refactor(project): Reduce number of files :art:
    I've merged some files to reduce unnecessary number of files within
    project.

--- handler/helpers_test.go
+++ handler/helpers_test.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"time"
 
+	"github.com/ddspog/mongo/elements"
 	"github.com/ddspog/mongo/model"
 	"github.com/globalsign/mgo/bson"
 )
@@ -101,6 +102,113 @@ func (p *product) CalculateCreatedOn() {
 // corresponding to actual time.
 func (p *product) CalculateUpdatedOn() {
 	p.UpdatedOnV = model.NowInMilli()
+}
+
+// productHandler it's an interface describing operations common to
+// handler's of MongoDB products collection.
+type productHandler interface {
+	Clean()
+	Link(elements.Databaser) error
+	Count() (int, error)
+	Find() (*product, error)
+	FindAll() ([]*product, error)
+	Insert() error
+	Remove() error
+	RemoveAll() (*elements.ChangeInfo, error)
+	Update(bson.ObjectId) error
+	Document() *product
+	SearchM() bson.M
+	Name() string
+}
+
+// productHandle it's a type embedding the Handle struct, it's capable
+// of storing products.
+type productHandle struct {
+	*Handle
+	DocumentV *product
+}
+
+// newProductHandle returns a empty productHandle.
+func newProductHandle() (p *productHandle) {
+	p = &productHandle{
+		Handle:    New(),
+		DocumentV: newProduct(),
+	}
+	return
+}
+
+// Name returns the name of connection that productHandle can connect.
+func (p *productHandle) Name() (n string) {
+	n = "products"
+	return
+}
+
+// Link connects the productHandle to collection.
+func (p *productHandle) Link(db elements.Databaser) (err error) {
+	err = p.Handle.Link(db, p.Name())
+	return
+}
+
+// Clean documents and search map values.
+func (p *productHandle) Clean() {
+	p.Handle.Clean()
+	p.DocumentV = newProduct()
+}
+
+// Find search on connected collection for a document matching data
+// stored on productHandle and returns it.
+func (p *productHandle) Find() (prod *product, err error) {
+	var doc model.Documenter = newProduct()
+	err = p.Handle.Find(p.Document(), doc)
+	prod = doc.(*product)
+	return
+}
+
+// FindAll search on connected collection for all documents matching
+// data stored on productHandle and returns it.
+func (p *productHandle) FindAll() (proda []*product, err error) {
+	var da []model.Documenter
+	err = p.Handle.FindAll(p.Document(), &da)
+	proda = make([]*product, len(da))
+	for i := range da {
+		//noinspection GoNilContainerIndexing
+		proda[i] = da[i].(*product)
+	}
+	return
+}
+
+// Insert creates a new document with data stored on productHandle
+// and put on connected collection.
+func (p *productHandle) Insert() (err error) {
+	err = p.Handle.Insert(p.Document())
+	return
+}
+
+// Remove delete a document from connected collection matching the id
+// of data stored on Handle.
+func (p *productHandle) Remove() (err error) {
+	err = p.Handle.Remove(p.Document().ID())
+	return
+}
+
+// Remove deletes all document from connected collection matching the
+// data stored on Handle.
+func (p *productHandle) RemoveAll() (info *elements.ChangeInfo, err error) {
+	info, err = p.Handle.RemoveAll(p.Document())
+	return
+}
+
+// Update updates document from connected collection matching the id
+// received, and uses document info to update.
+func (p *productHandle) Update(id bson.ObjectId) (err error) {
+	err = p.Handle.Update(id, p.Document())
+	return
+}
+
+// Document returns the Document of Handle with correct type.
+func (p *productHandle) Document() (d *product) {
+	d = p.DocumentV
+	return
 }
 
 // Finish call Finish() from all objects received.
