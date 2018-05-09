a788353bd73d5ca4750a2c71b5a6beba70f59c60
Dênnis Dantas <ddspog@gmail.com>
2018-05-08 21:46:11 -0300

    feat(connecters): Add internal connecters package :bulb:
    I've created the connecters internal package. It implements two versions
    of MongoConnecter, one to production and the other for test. The first
    one use the same functions used on v1 of mongo package connect.go file.
    The testable version use mgo.dbtest DBServer to create a temporary
    database initialized with choosen documents, among other features.

---
+++ internal/connecter/testmongo.go
@@ -0,0 +1,132 @@
+package connecter
+
+import (
+	"errors"
+	"io/ioutil"
+	"reflect"
+	"strings"
+
+	"github.com/globalsign/mgo"
+	"github.com/globalsign/mgo/dbtest"
+)
+
+var (
+	// ErrInvalidFixtureKeys it's an error received when keys on
+	// fixture doesn't follow correct structure <col.id>.
+	ErrInvalidFixtureKeys = errors.New("invalid fixture key name, use <col.id>")
+	// ErrInvalidFixtureMap it's an error received when fixture
+	// received can't be converted to a map.
+	ErrInvalidFixtureMap = errors.New("fixtures received ain't a map")
+)
+
+// TestMongo is a MongoConnecter with use on testing purposes. Using a
+// dbtest.DBServer to simulate a Database on a temp directory.
+type TestMongo struct {
+	dir      string
+	prefix   string
+	session  *mgo.Session
+	server   *dbtest.DBServer
+	fixtures interface{}
+	resetFn  *func() error
+}
+
+// NewTestable returns a TestMongo as MongoConnecter, using a temp
+// directory, fixtures to init the database and a optional reset
+// function address, to be set when connecting the MongoConnecter.
+//
+// It's important to send fixtures as a map[string] of any type,
+// otherwise, an error will be returned on Connect(). Also, every
+// element must have a key structure as <col.id> for identification
+// of collection to insert.
+func NewTestable(d, p string, fixtures interface{}, reset ...*func() error) (m MongoConnecter) {
+	tm := &TestMongo{
+		dir:      d,
+		prefix:   p,
+		fixtures: fixtures,
+	}
+
+	if len(reset) == 1 {
+		tm.resetFn = reset[0]
+	}
+
+	tm.server = new(dbtest.DBServer)
+	m = tm
+	return
+}
+
+// Connect to MongoDB of server. It tries to connect with MONGODB_URL,
+// but without defining this environment variable, tris to connect
+// with default URL. It also defines a reset function if received on
+// constructor, to reset and initialize database with fixtures.
+func (m *TestMongo) Connect() (err error) {
+	var path string
+	if path, err = ioutil.TempDir(m.dir, m.prefix); err == nil {
+		m.server.SetPath(path)
+		m.session = m.server.Session()
+
+		// No errors showing, save objects.
+		m.session.SetSafe(&mgo.Safe{})
+
+		if err = m.insertFixtures(); err == nil && m.resetFn != nil {
+			*m.resetFn = func() (err error) {
+				m.Session().DB("test").DropDatabase()
+				err = m.insertFixtures()
+				return
+			}
+		}
+	}
+
+	return
+}
+
+// insertFixtures init the database with some documents, defined on the
+// constructor as fixtures.
+func (m *TestMongo) insertFixtures() (err error) {
+	if v := reflect.ValueOf(m.fixtures); v.Kind() == reflect.Map {
+		for _, key := range v.MapKeys() {
+			doc := v.MapIndex(key)
+
+			names := strings.Split(key.String(), ".")
+			if len(names) == 2 {
+				err = m.Session().DB("test").C(names[0]).Insert(doc.Interface())
+			} else {
+				err = ErrInvalidFixtureKeys
+			}
+
+			if err != nil {
+				return
+			}
+		}
+	} else {
+		err = ErrInvalidFixtureMap
+	}
+	return
+}
+
+// Disconnect undo the connection made. Preparing package for a new
+// connection.
+func (m *TestMongo) Disconnect() {
+	m.session.Close()
+	m.session = nil
+	m.server.Stop()
+}
+
+// ConsumeDatabaseOnSession clones a session and use it to creates a
+// Databaser object to be consumed in f function. Closes session after
+// consume of Databaser object. Returns nil if no session is available.
+func (m *TestMongo) ConsumeDatabaseOnSession(f func(*mgo.Database)) {
+	if s := m.Session(); s != nil {
+		s := s.Clone()
+		defer s.Close()
+
+		f(s.DB("test"))
+	} else {
+		f(nil)
+	}
+}
+
+// Session return connected mongo session.
+func (m *TestMongo) Session() (s *mgo.Session) {
+	s = m.session
+	return
+}
---
+++ internal/connecter/testmongo_test.go
@@ -0,0 +1,158 @@
+// +build !acceptance
+
+package connecter
+
+import (
+	"testing"
+
+	"github.com/ddspog/mspec/bdd"
+	"github.com/globalsign/mgo"
+	"github.com/globalsign/mgo/bson"
+)
+
+// Feature TstMongo accept only map fixtures
+// - As a developer,
+// - I want to be able to use Testmongo only with map fixtures,
+// - So that I can access info on keys and data of each fixture.
+func Test_TstMongo_accept_only_map_fixtures(t *testing.T) {
+	given, _, _ := bdd.Sentences()
+
+	given(t, "a new test MongoConnecter m with an array as fixtures", func(when bdd.When, args ...interface{}) {
+		mc := NewTestable("", "internalTesting", []interface{}{newProduct(), newProduct()})
+
+		err := mc.Connect()
+		defer mc.Disconnect()
+
+		when("err := mc.Connect() is called", func(it bdd.It) {
+			it("should return an error", func(assert bdd.Assert) {
+				assert.Equal(ErrInvalidFixtureMap, err)
+			})
+		})
+	})
+}
+
+// Feature TstMongo accept only correct fixtures
+// - As a developer,
+// - I want to be able to use Testmongo only with correct naming fixtures,
+// - So that I can differentiate collections withing Testmongo fixtures.
+func Test_TstMongo_accept_only_correct_fixtures(t *testing.T) {
+	given, like, s := bdd.Sentences()
+
+	given(t, "a new test MongoConnecter m with %[1]v style of fixtures key naming", func(when bdd.When, args ...interface{}) {
+		mc := NewTestable("", "internalTesting", args[2].(map[string]interface{}))
+		err := mc.Connect()
+		defer mc.Disconnect()
+
+		when("err := mc.Connect() is called", func(it bdd.It) {
+
+			if args[1].(bool) {
+				it("shouldn't return any error", func(assert bdd.Assert) {
+					assert.Nil(err)
+				})
+			} else {
+				it("should return an error", func(assert bdd.Assert) {
+					assert.Equal(ErrInvalidFixtureKeys, err)
+				})
+			}
+		})
+	}, like(
+		s("<db.col.id>", false, dbColIdFixtures),
+		s("<col.id>", true, colIdFixtures),
+		s("<id>", false, idFixtures),
+	))
+}
+
+// Feature TstMongo sets a reset function
+// - As a developer,
+// - I want to be able to use Testmongo and set a reset function,
+// - So that I can reset the temp database between tests.
+func Test_TstMongo_sets_a_reset_function(t *testing.T) {
+	given, _, _ := bdd.Sentences()
+
+	given(t, "a new test MongoConnecter m with a function 'Reset'", func(when bdd.When) {
+		var Reset func() error
+		mc := NewTestable("", "internalTesting", colIdFixtures, &Reset)
+		err := mc.Connect()
+		defer mc.Disconnect()
+
+		when("err := mc.Connect() is called", func(it bdd.It) {
+			it("shouldn't return any error", func(assert bdd.Assert) {
+				assert.Nil(err)
+			})
+		})
+
+		err = Reset()
+
+		when("err := Reset() is called", func(it bdd.It) {
+			it("shouldn't return any error", func(assert bdd.Assert) {
+				assert.Nil(err)
+			})
+		})
+	})
+}
+
+// Feature TstMongo consume database on session
+// - As a developer,
+// - I want to be able to use Testmongo to get database object between sessions,
+// - So that I can use it in parallel in isolate cases.
+func Test_TstMongo_consume_database_on_session(t *testing.T) {
+	given, _, _ := bdd.Sentences()
+
+	given(t, "a new test MongoConnecter m, and DBCollecter(el){db = el}", func(when bdd.When) {
+		mc := NewTestable("", "internalTesting", colIdFixtures)
+
+		var db *mgo.Database
+		DBCollecter := func(el *mgo.Database) {
+			db = el
+		}
+
+		mc.ConsumeDatabaseOnSession(DBCollecter)
+
+		when("mc.ConsumeDatabaseOnSession(DBCollecter) is called before mc.Connect()", func(it bdd.It) {
+			it("db should still be nil", func(assert bdd.Assert) {
+				assert.Nil(db)
+			})
+		})
+
+		mc.Connect()
+		defer mc.Disconnect()
+
+		mc.ConsumeDatabaseOnSession(DBCollecter)
+
+		when("mc.ConsumeDatabaseOnSession(DBCollecter) is called after mc.Connect()", func(it bdd.It) {
+			it("db should partially equal to mc.Session().DB('test')", func(assert bdd.Assert) {
+				exp := mc.Session().DB("test")
+				assert.Equal(exp.Name, db.Name)
+			})
+		})
+	})
+}
+
+var (
+	// Various fixtures to help on tests.
+	dbColIdFixtures = map[string]interface{}{
+		"test.products.testid1": newProduct(),
+		"test.products.testid2": newProduct(),
+	}
+	colIdFixtures = map[string]interface{}{
+		"products.testid1": newProduct(),
+		"products.testid2": newProduct(),
+	}
+	idFixtures = map[string]interface{}{
+		"testid1": newProduct(),
+		"testid2": newProduct(),
+	}
+)
+
+// product it's an random type.
+type product struct {
+	IDV bson.ObjectId `json:"_id,omitempty" bson:"_id,omitempty"`
+}
+
+// newProduct creates a product with random id.
+func newProduct() (p *product) {
+	p = &product{
+		IDV: bson.NewObjectId(),
+	}
+	return
+}
